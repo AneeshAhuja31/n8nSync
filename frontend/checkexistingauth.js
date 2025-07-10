5cbf2d4368a3c0046682a3ceb2b156f68eb6a5fc
Aneesh Ahuja <aneeshahuja31@gmail.com>
2025-07-11 04:26:23 +0530

    fixed window.localStorage to window.location

--- frontend/checkexistingauth.js
+++ frontend/checkexistingauth.js
@@ -1,14 +1,13 @@
 async function checkexistingAuth() {
     try{
-        const response = await fetch("http://localhost:8000/auth/validate/",{
+        const response = await fetch("http://localhost:8000/auth/validate",{
             method:"GET",
             credentials:"include" //include cookies
         })
-
         if(response.ok){
             const data = await response.json();
             if(data.valid){
-                window.localStorage.href = 'http://localhost:3000/dashboard.html'
+                window.location.href = 'http://localhost:3000/dashboard.html'
                 return;
             }
         }
